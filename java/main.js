6158bd12c1b73115f5ff00e4f556c2eba64a6eef
Amirni19 <amir19nigmatullin.19@gmail.com>
2024-09-11 19:00:36 +0300

    practis 4

---
+++ java/main.js
@@ -0,0 +1,44 @@
+'use strict';
+
+// let a = prompt('Напишите ');
+// for (let i = 2; i <= 10; i++) {
+//     if (i % 2 == 0) {
+//       alert( i );
+//     }
+// }
+
+// let i = 0;
+// while (i < 3){
+//     alert(i);
+//     i++;
+// }
+  
+// let num;
+// do {
+//   num = prompt("Введите число");
+// } while (num <= 10 && num);
+
+// let a = prompt('Введите первое число');
+// let b = prompt('Ведите второе число');
+// function min(a, b){
+//     if (a < b) {
+//         return a;
+//       } else {
+//         return b;
+//       }
+// }
+
+// function ask(question, yes, no) {
+//   if (confirm(question)) yes()
+//   else no();
+// }
+// ask(
+//   "Вы согласны?",
+//   () => alert("Вы согласились."),
+//   () => alert("Вы отменили выполнение.")
+// )
+
+// function checkAge(age) {
+//     return (age > 18) || confirm('Родители разрешили');
+//   }
+  
